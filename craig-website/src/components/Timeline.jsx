d9d433cb987f795e70e0c531a65f879b3ec344fe
Cracov <craig.a.coventry@gmail.com>
2023-05-07 22:12:31 +0100

    Working on website

--- craig-website/src/components/Timeline.jsx
+++ craig-website/src/components/Timeline.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import timeline from '../data/timeline';
+import timelineData from '../data/Timeline';
 import TimelineItem from './TimelineItem';
 import Title from './Title';
 
 function Timeline() {
    return (
-      <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="flex flex-col md:flex-row justify-center my-20\">
          <div className="w-full md:w-7/12">
             <Title>Timeline</Title>
-            {timeline.map(item => (
+            {timelineData.map(item => (
                <TimelineItem 
                   year={item.year}
                   title={item.title}
